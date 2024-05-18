31f3f5b75dd6c5ffd82b3ca7d71b0094264659eb
jatin chary <charyjatin@gmail.com>
2024-05-18 17:58:11 +0530

    again i say

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "./Context/theme";
-import About from "./components/About/About";
-import Projects from "./components/Projects/Projects";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
